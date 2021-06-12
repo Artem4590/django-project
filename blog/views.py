6c6db07b23b0c0c0f5ce616aaee9746c1d457a3a
Artem4590 <lavryha4590@gmail.com>
2021-06-12 21:27:26 +0300

    Добавление пагинации на странице постов

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,46 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # обработчик для списка постов
 def post_list(request):
     # с помощью менеджера модели, получаем все опубликованные посты
-    posts = Post.published.all()
+    # posts = Post.published.all()
+
+    object_list = Post.published.all()
+
+    # Постраничное отображение работает следующим образом:
+
+    # 1) мы инициализируем объект класса Paginator, указав количество объектов на одной странице
+    # по 3 статьи на каждой станице
+    paginator = Paginator(object_list, 3)
+
+    # 2) извлекаем из запроса GET-параметр page, который указывает текущую страницу;
+    page = request.GET.get('page')
+    try:
+        # 3) получаем список объектов на нужной странице с помощью метода page()класса Paginator
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # 4.1) если указанный параметр page не является целым числом, обращаемся к первой странице.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # 4.2) если page больше, чем общее количество страниц, то возвращаем последнюю
+        posts = paginator.page(paginator.num_pages)
+
+    # 5) передаем номер страницы и полученные объекты в шаблон через render(... {'page': page, ...})
 
     # Используем функцию render() для формирования шабло-на со списком статей
     # Она принимает объект запроса request, путь к шаблону и переменные контекста для этого шаблона
     # В ответ вернется объект HttpRe-sponse со сформированным текстом
     # Функция render()использует переданный контекст при формировании шаблона, поэтому любая переменная контекста будет доступна в шаблоне
     # Процессоры контекста – это вызываемые функции, которые добавляют в контекст переменные
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 # обработчик страницы поста
 # Он принимает на вход аргументы year, month, day и post для получения статьи по указанным слагу и дате
 def post_detail(request, year, month, day, post):
-    
+
     # функция возвращает объект, который подходит по указанным параметрам, или вызывает исключение HTTP 404
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
 
