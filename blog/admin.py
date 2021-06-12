90689164be40306bab6317550164c3b785e73882
Artem4590 <lavryha4590@gmail.com>
2021-06-12 17:22:21 +0300

    Настроил отображение модели Post в админке

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,32 @@
+# регистрация моделей для отображения в админке
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+# admin.site.register(Post)
+
+# Декоратор @admin.register() выполняет те же действия, что и функция admin.site.register(): регистрирует декорируе-мый класс – наследник ModelAdmin
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # настройка отображения модели
+
+    # озволяет перечислить поля модели, которые мы хотим отображать на странице списка
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+
+    # Справа на странице появился блок фильтрации  списка,  который  фильтрует  статьи  по  полям,  перечисленным в list_filter
+    list_filter = ('status', 'created', 'publish', 'author')
+
+    # Также появилась строка поиска. Она добавляется для моделей, для которых определен атрибут search_fields
+    search_fields = ('title', 'body')
+
+    # slug генерируется авто-матически из поля title с помощью атрибута prepopulated_fields
+    prepopulated_fields = {'slug': ('title',)}
+
+    # Также теперь поле author содержит поле поиска, что значительно упрощает выбор автора из выпадающего списка, когда в системе сотни пользователей
+    raw_id_fields = ('author',)
+
+    # Под поиском благодаря атрибу-ту date_hierarchy добавлены ссылки для навигации по датам
+    date_hierarchy = 'publish'
+
+    # По умолчанию статьи  отсортированы по полям status и publish. Эта настройка задается в атри-буте ordering
+    ordering = ('status', 'publish')
